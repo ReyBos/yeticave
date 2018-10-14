b35127cca99731656907ce4b4aa8a78546c61dff
ReyBos <andreybosiy@yandex.ru>
2018-10-14 11:24:02 +0300

    Выполены задания 3 раздела

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+//Форматирование суммы >999 к виду 135 000р
+function format_cost($cost) {
+    $int_cost = ceil($cost);
+    if ($int_cost >= 1000) {
+        $beginning_cost = substr((string)$int_cost, 0, -3);
+        $ending_cost = substr((string)$int_cost, -3);
+        $format_cost = $beginning_cost . " " . $ending_cost;
+    } else {
+        $format_cost = $int_cost;
+    }
+
+    $format_cost .= '<b class="rub">р</b>';
+    return $format_cost;
+}
+
+//Возвращает строку - итоговый HTML-код с подставленными данными
+function render_template($file_name, $data_array) {
+    $html = "";
+    if (file_exists($file_name)) {
+        ob_start();
+        require($file_name);
+        $html = ob_get_clean();
+
+        return $html;
+    } else {
+        $html = "";
+        return $html;
+    }
+}
